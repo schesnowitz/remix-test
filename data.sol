7ce5c6fc2c320d18c10034181b80de8dcf01fa17
schesnowitz <steve@margoscargo.com>
2023-02-10 22:07:59 -0500

    data

---
+++ data.sol
@@ -0,0 +1,8 @@
+pragma solidity >= 0.4.22 <= 0.8.17;
+
+contract DataTypes {
+    uint8 public smallNumber = 9;
+    int8 public negativeNumber = -7;
+    address public zeroAddress;
+    bool public canEdit = true;
+}
